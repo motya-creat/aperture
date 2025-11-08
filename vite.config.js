6f0048fc06695e7cbb99d8acea378671bd2a0aa9
Your Name <you@example.com>
2025-11-08 22:58:05 +0700

    fix: GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
+import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  base: '/aperture/',
+  plugins: [
+    ghPages({
+      base: process.env.NODE_ENV === 'production' ? '/<aperture>/' : '/',
+      repo: '<motya-creat>/<aperture>',
+      branch: 'gh-pages',
+    }),
+  ],
 });
